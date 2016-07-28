f0ec41f2c2d6b53f69f025a2a0dba4d952d93f59
James Youn <jamesyoun710@gmail.com>
2016-07-27 17:26:50 -0700

    Add specs & npm

---
+++ specs/specs.js
@@ -0,0 +1,8 @@
+var tape = require('tape')
+var script = require('../bin/tetris')
+
+// TODO: use client side test library!
+// Tape not supported
+tape('smoke test', function(assert) {
+  assert.equal(true, false)
+})
